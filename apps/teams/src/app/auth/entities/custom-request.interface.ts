e14914f8d774089f21794aacf5a475d99deeae9c
flamurjahiri <flamurjahiri@kodelabs.com>
2024-01-21 14:49:29 +0100

    added more validation on middleware

--- apps/teams/src/app/auth/entities/custom-request.interface.ts
+++ apps/teams/src/app/auth/entities/custom-request.interface.ts
@@ -1,5 +1,9 @@
 import {Request} from "express"
+import {User} from "../../users/entities/users.schema";
+import {UserSimple} from "../../users/dto/user.simple";
 
 export interface HttpRequest extends Request {
   id: string;
+  user: User;
+  userSimple: UserSimple
 }
--- apps/teams/src/app/providers/middleware/request-user-id.middleware.ts
+++ apps/teams/src/app/providers/middleware/request-user-id.middleware.ts
@@ -1,19 +1,27 @@
-import {Inject, Injectable, NestMiddleware} from "@nestjs/common";
-import {map, of, tap} from "rxjs";
+import {ForbiddenException, Inject, Injectable, NestMiddleware} from "@nestjs/common";
+import {iif, map, mergeMap, of, tap, throwError} from "rxjs";
 import {HttpRequest} from "../../auth/entities/custom-request.interface";
 import {AuthRedisService} from "../../auth/services/auth.redis.service";
+import {UserService} from "../../users/services/user.service";
+import {UserSimple} from "../../users/dto/user.simple";
 
 @Injectable()
 export class RequestUserIdMiddleware implements NestMiddleware {
   @Inject(AuthRedisService) authRedisStore: AuthRedisService
-
+  @Inject(UserService) userService: UserService
 
   use(req: HttpRequest, res: Response, next: (error?: any) => void): any {
 
     if (req?.headers?.authorization) {
       return of(req.headers.authorization).pipe(
         map(header => header.split("-userId:")[1].replace("-userId:", "")),
-        tap(id => req.id = id)
+        mergeMap(id => this.userService.getById(id)),
+        tap(u => {
+          req.id = u._id;
+          req.user = u;
+          req.userSimple = new UserSimple(u);
+        }),
+        mergeMap(u => iif(() => u.validated.email || u.validated.phoneNumber, of(u), throwError(() => new ForbiddenException("Please verify email or password"))))
       ).subscribe({next: () => next(), error: (err) => next(err)})
     }
 
