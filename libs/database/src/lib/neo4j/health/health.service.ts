ff7da93da1eb5c55c2958a0bb84ad853fcbc7878
flamurjahiri <flamurjahiri@kodelabs.com>
2024-06-23 14:11:42 +0200

    on module destroy

--- libs/database/src/lib/neo4j/health/health.service.ts
+++ libs/database/src/lib/neo4j/health/health.service.ts
@@ -1,11 +1,15 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnModuleDestroy } from '@nestjs/common';
 import { HealthCheckError, HealthIndicator, HealthIndicatorResult } from '@nestjs/terminus';
 import { Driver } from 'neo4j-driver';
 import { NEO_4J_DRIVER } from '../assets/constants';
 
 @Injectable()
-export class Neo4JHealthService extends HealthIndicator {
+export class Neo4JHealthService extends HealthIndicator implements OnModuleDestroy {
   @Inject(NEO_4J_DRIVER) private readonly driver: Driver;
+
+  async onModuleDestroy(): Promise<void> {
+    return this.driver?.close();
+  }
 
   async check(): Promise<HealthIndicatorResult> {
     const startTime = new Date();
