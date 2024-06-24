34209b968e2150a9e73ec0c1962eca0499af48b9
flamurjahiri <flamurjahiri@kodelabs.com>
2024-06-24 20:14:01 +0200

    export

--- libs/database/src/lib/neo4j/index.ts
+++ libs/database/src/lib/neo4j/index.ts
@@ -4,3 +4,4 @@ export * from './health/health.service';
 export * from './entities/neo4j.config';
 export * from './entities/neo4j.operation.type';
 export * from './entities/neo4j.factory.config'
+export * from './services/neo4j.database.service'
