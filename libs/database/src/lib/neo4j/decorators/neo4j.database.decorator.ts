a29d0160bb7d1118fcea268614d977bf7cf1a5c3
flamurjahiri <flamurjahiri@kodelabs.com>
2024-07-02 19:37:36 +0200

    inject models

--- libs/database/src/lib/neo4j/decorators/neo4j.database.decorator.ts
+++ libs/database/src/lib/neo4j/decorators/neo4j.database.decorator.ts
@@ -2,14 +2,14 @@ import { Inject } from '@nestjs/common';
 import { NEO_4J_CONNECTION_DRIVER, NEO_4J_DATABASE, NEO_4J_HEALTH_CHECK } from '../assets/constants';
 
 
-export function Neo4jDatabase(database: string, connectionName?: string) {
+export function InjectDatabase(database: string, connectionName?: string) {
   return Inject(NEO_4J_DATABASE(database, connectionName));
 }
 
-export function Neo4jDriver(connectionName?: string) {
+export function InjectDriver(connectionName?: string) {
   return Inject(NEO_4J_CONNECTION_DRIVER(connectionName));
 }
 
-export function Neo4jHealth(connectionName?: string) {
+export function InjectHealth(connectionName?: string) {
   return Inject(NEO_4J_HEALTH_CHECK(connectionName));
 }
