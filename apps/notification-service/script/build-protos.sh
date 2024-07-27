85e8aef9859b0f2cda8bce600bd9c9672308338b
flamurjahiri <flamurjahiri@kodelabs.com>
2024-07-27 14:28:33 +0200

    proto

--- apps/notification-service/script/build-protos.sh
+++ apps/notification-service/script/build-protos.sh
@@ -7,6 +7,7 @@ PROTO_DEST=./src/app/generated-protos
 
 mkdir -p ${PROTO_DEST}
 
-
+apt-get update -y
+apt-get install protobuf-compiler -y
 npm install -g protoc-gen-ts
 protoc -I=./src/app/protos --ts_out=${PROTO_DEST} ./src/app/protos/*.proto
